983703ac35f36cd1151701a181054b567f67824f
Roman <romazaniosdev@gmail.com>
2026-06-09 13:03:58 +0300

    Initial Commit

---
+++ 2AIVideoGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2AIVideoGenerator
+//
+//  Created by Roman on 6/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2AIVideoGenerator/_AIVideoGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  _AIVideoGeneratorApp.swift
+//  2AIVideoGenerator
+//
+//  Created by Roman on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct _AIVideoGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
